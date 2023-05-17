bb3d4bd1e97565fe56c3fc2742fc06270690c2e7
Mintan_Lathiya <lathiyamintan1998@gmail.com>
2023-05-17 16:35:16 +0530

    first_commit

--- lib/favoriteroomui_demo1.dart
+++ lib/favoriteroomui_demo1.dart
@@ -5,19 +5,22 @@ class FavoriteRoom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Column(
         children: [
-          Container(
-            height: 500,
-            width: 250,
-            decoration: const BoxDecoration(
-              color: Colors.black,
-              image: DecorationImage(
-                image: AssetImage('assets/images/1.jpeg'),
-              ),
-            ),
+          Image(
+            image: AssetImage('assets/images/1.jpeg'),
           ),
+          // Container(
+          //   height: 500,
+          //   width: 250,
+          //   decoration: const BoxDecoration(
+          //     color: Colors.black,
+          //     image: DecorationImage(
+          //       image: AssetImage('assets/images/1.jpeg'),
+          //     ),
+          //   ),
+          // ),
         ],
       ),
     );
