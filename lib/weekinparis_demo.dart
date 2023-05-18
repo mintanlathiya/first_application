f443fa30561defbab6d81e639fb9172ebfa10908
Mintan_Lathiya <lathiyamintan1998@gmail.com>
2023-05-18 09:01:39 +0530

    firstcommit

--- lib/weekinparis_demo.dart
+++ lib/weekinparis_demo.dart
@@ -12,95 +12,134 @@ class WeekInParisUi extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Icon(Icons.sort),
-              Icon(Icons.search),
-            ],
-          ),
-          const Row(
-            children: [
-              Text(
-                'Week in Paris',
-                style: TextStyle(
-                    fontSize: 50,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.pink),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Icon(Icons.search),
               ),
             ],
           ),
-          const Row(
-            children: [
-              Text('2021 fashion show in Pairs'),
-            ],
-          ),
-          const Row(
-            children: [
-              Expanded(
-                child: Text(
-                  'Explore',
+          const Padding(
+            padding: EdgeInsets.only(left: 15),
+            child: Row(
+              children: [
+                Text(
+                  'Week in Paris',
                   style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.pink),
+                ),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 15),
+            child: Row(
+              children: [
+                Text('2021 fashion show in Pairs'),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 15),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'Explore',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              Icon(
-                Icons.info_sharp,
-              )
-            ],
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.info_sharp,
+                  ),
+                )
+              ],
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    height: 300,
-                    width: 250,
-                    decoration: const BoxDecoration(
-                        color: Colors.green,
-                        // image: DecorationImage(image: NetworkImage('')),
-                        borderRadius: BorderRadius.all(Radius.circular(20))),
-                  ),
-                  const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Niketa William'),
-                    ],
-                  ),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text('Paris'),
-                    ],
-                  ),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    height: 300,
-                    width: 250,
-                    decoration: const BoxDecoration(
-                        color: Colors.green,
-                        // image: DecorationImage(image: NetworkImage('')),
-                        borderRadius: BorderRadius.all(Radius.circular(20))),
-                  ),
-                  const Text('Trisha Louis'),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('London'),
-                    ],
-                  ),
-                ],
-              )
-            ],
+          const Padding(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  'Recommended',
+                  style: TextStyle(
+                      color: Colors.purple, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'New Models',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  '2020 Show',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 300,
+                      width: 250,
+                      decoration: const BoxDecoration(
+                          color: Colors.green,
+                          // image: DecorationImage(image: NetworkImage('')),
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                    ),
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Niketa William'),
+                      ],
+                    ),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text('Paris'),
+                      ],
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 300,
+                      width: 250,
+                      decoration: const BoxDecoration(
+                          color: Colors.green,
+                          // image: DecorationImage(image: NetworkImage('')),
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                    ),
+                    const Text('Trisha Louis'),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('London'),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
           Container(
-            margin: const EdgeInsets.all(50),
-            height: 250,
+            margin: const EdgeInsets.all(45),
+            height: 230,
             width: 540,
             decoration: const BoxDecoration(
                 color: Colors.green,
