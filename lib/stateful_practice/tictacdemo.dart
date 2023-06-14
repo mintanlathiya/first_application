d46eaa023b11c39dea1bdd7524e96504a7ee133f
Mintan_Lathiya <lathiyamintan1998@gmail.com>
2023-06-14 20:52:59 +0530

    first commit

---
+++ lib/stateful_practice/tictacdemo.dart
@@ -0,0 +1,132 @@
+import 'package:first_application/statefullexample/counter.controller.dart';
+import 'package:flutter/material.dart';
+
+class TicTacDemoUi extends StatefulWidget {
+  const TicTacDemoUi({super.key});
+
+  @override
+  State<TicTacDemoUi> createState() => _TicTacDemoUiState();
+}
+
+class _TicTacDemoUiState extends State<TicTacDemoUi> {
+  @override
+  Widget build(BuildContext contect) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    const Text(
+                      'PLAYER 1',
+                      style: TextStyle(fontSize: 45),
+                    ),
+                    Text(
+                      'Score ${oScore.toString()}',
+                      style: const TextStyle(fontSize: 45),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    const Text(
+                      'PLAYER 2',
+                      style: TextStyle(fontSize: 45),
+                    ),
+                    Text(
+                      'Score ${xScore.toString()}',
+                      style: const TextStyle(fontSize: 45),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Expanded(
+                child: GridView.builder(
+              itemCount: 9,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3, mainAxisSpacing: 5, crossAxisSpacing: 5),
+              itemBuilder: (context, index) => InkWell(
+                onTap: () {
+                  tapped(index, context);
+                  setState(() {});
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      ),
+                      color: Colors.red,
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(20))),
+                  child: Text(
+                    displayElement[index],
+
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 80,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                MaterialButton(
+                  color: Colors.red,
+                  height: 80,
+                  minWidth: 220,
+                  shape: const BeveledRectangleBorder(
+                      side: BorderSide(),
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  onPressed: () {
+                    clearScoreBoard();
+                    setState(() {});
+                  },
+                  child: const Text(
+                    " clear Score Board",
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                MaterialButton(
+                  color: Colors.red,
+                  height: 80,
+                  minWidth: 220,
+                  shape: const BeveledRectangleBorder(
+                      side: BorderSide(),
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  onPressed: () {
+                    clearBoard();
+
+                    setState(() {});
+                  },
+                  child: const Text(
+                    "Restart",
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/statefullexample/counter.controller.dart
+++ lib/statefullexample/counter.controller.dart
@@ -77,7 +77,25 @@ class ColorSwap {
   }
 }
 
+//   static Color colorChangeData(int index) {
+//     if (index % 2 == 0) {
+//       return colors[0];
+//     } else {
+//       Color temp = colors[0];
+//       colors[0] = colors[1];
+//       colors[1] = temp;
+//       return colors[0];
+//     }
+//   }
 
+// static Color colorChangeIndex(int index) {
+//   if (index == number[count]) {
+//     count++;
+//     return Colors.red;
+//   } else {
+//     return Colors.green;
+//   }
+//
 class CrossColor {
   static List data = List.generate(20, (index) => index);
   static List outputData = [];
@@ -91,13 +109,222 @@ class CrossColor {
   }
 }
 
-// class ZeroCancle {
-//   IconData? zero, cancle;
-//   static void playGame(int index) {
-//     if (index % 2 == 0) {
-//       print(Icons.exposure_zero);
-//     } else {
-//       print(Icons.close);
-//     }
-//   }
-// }
+String player1 = "Player O";
+String player2 = "Player X";
+bool isActivated = false; // true means player O
+
+int playerOCount = 0;
+int playerXCount = 0;
+int drawMatch = 0;
+
+List value = ["", "", "", "", "", "", "", "", ""];
+
+class TicToeTac {
+  static void clearControl() {
+    value = ["", "", "", "", "", "", "", "", ""];
+  }
+
+  static void winnerNumber(context) {
+    (isActivated) ? playerXCount++ : playerOCount++;
+
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content:
+            Text((isActivated) ? "Player X is Winner" : "Player O is Winner")));
+    clearControl();
+  }
+
+  static void playerSwapping(int index) {
+    if (value[index] == "") {
+      if (isActivated == true) {
+        value.removeAt(index);
+        value.insert(index, "O");
+        isActivated = false;
+      } else {
+        value.removeAt(index);
+        value.insert(index, "X");
+        isActivated = true;
+      }
+    }
+  }
+
+  static void winnerState(context) {
+    if (value[0] == value[1] && value[1] == value[2] && value[0] != "") {
+      winnerNumber(context);
+    } else if (value[3] == value[4] && value[4] == value[5] && value[4] != "") {
+      winnerNumber(context);
+    } else if (value[6] == value[7] && value[7] == value[8] && value[8] != "") {
+      winnerNumber(context);
+    } else if (value[0] == value[3] && value[3] == value[6] && value[0] != "") {
+      winnerNumber(context);
+    } else if (value[1] == value[4] && value[4] == value[7] && value[1] != "") {
+      winnerNumber(context);
+    } else if (value[2] == value[5] && value[5] == value[8] && value[2] != "") {
+      winnerNumber(context);
+    } else if (value[0] == value[4] && value[4] == value[8] && value[0] != "") {
+      winnerNumber(context);
+    } else if (value[2] == value[4] && value[4] == value[6] && value[2] != "") {
+      winnerNumber(context);
+    } else if (value[0] != "" &&
+        value[1] != "" &&
+        value[2] != "" &&
+        value[3] != "" &&
+        value[4] != "" &&
+        value[5] != "" &&
+        value[6] != "" &&
+        value[7] != "" &&
+        value[8] != "") {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Draw Match Play Again..."),
+        ),
+      );
+      drawMatch += 1;
+      clearControl();
+    }
+  }
+}
+
+
+  bool oTurn = true;
+
+  List displayElement = ['', '', '', '', '', '', '', '', ''];
+
+  int oScore = 0;
+  int xScore = 0;
+  int filledBoxes = 0;
+
+  void tapped(int index, context) {
+    if (oTurn && displayElement[index] == '') {
+      displayElement[index] = 'O';
+      filledBoxes++;
+    } else if (!oTurn && displayElement[index] == '') {
+      displayElement[index] = 'X';
+      filledBoxes++;
+    }
+
+    oTurn = !oTurn;
+    _checkWinner(context);
+  }
+
+  void _checkWinner(context) {
+    // Checking rows
+    if (displayElement[0] == displayElement[1] &&
+        displayElement[0] == displayElement[2] &&
+        displayElement[0] != '') {
+      _showWinDialog(displayElement[0], context);
+      clearBoard();
+    }
+    if (displayElement[3] == displayElement[4] &&
+        displayElement[3] == displayElement[5] &&
+        displayElement[3] != '') {
+      _showWinDialog(displayElement[3], context);
+      clearBoard();
+    }
+    if (displayElement[6] == displayElement[7] &&
+        displayElement[6] == displayElement[8] &&
+        displayElement[6] != '') {
+      _showWinDialog(displayElement[6], context);
+      clearBoard();
+    }
+
+    // Checking Column
+    if (displayElement[0] == displayElement[3] &&
+        displayElement[0] == displayElement[6] &&
+        displayElement[0] != '') {
+      _showWinDialog(displayElement[0], context);
+      clearBoard();
+    }
+    if (displayElement[1] == displayElement[4] &&
+        displayElement[1] == displayElement[7] &&
+        displayElement[1] != '') {
+      _showWinDialog(displayElement[1], context);
+      clearBoard();
+    }
+    if (displayElement[2] == displayElement[5] &&
+        displayElement[2] == displayElement[8] &&
+        displayElement[2] != '') {
+      _showWinDialog(displayElement[2], context);
+      clearBoard();
+    }
+
+    // Checking Diagonal
+    if (displayElement[0] == displayElement[4] &&
+        displayElement[0] == displayElement[8] &&
+        displayElement[0] != '') {
+      _showWinDialog(displayElement[0], context);
+      clearBoard();
+    }
+    if (displayElement[2] == displayElement[4] &&
+        displayElement[2] == displayElement[6] &&
+        displayElement[2] != '') {
+      _showWinDialog(displayElement[2], context);
+      clearBoard();
+    } else if (filledBoxes == 9) {
+      _showDrawDialog(context);
+      clearBoard();
+    }
+  }
+
+  void _showWinDialog(String winner, context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("\" $winner \" is Winner!!!"),
+            actions: [
+              TextButton(
+                child: const Text("Play Again"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+
+    if (winner == 'O') {
+      oScore++;
+    } else if (winner == 'X') {
+      xScore++;
+    }
+  }
+
+  void _showDrawDialog(context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Draw"),
+            actions: [
+              TextButton(
+                child: const Text("Play Again"),
+                onPressed: () {
+                  clearBoard();
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  void clearBoard() {
+    for (int i = 0; i < 9; i++) {
+      displayElement[i] = '';
+    }
+
+    filledBoxes = 0;
+  }
+
+  void clearScoreBoard() {
+    xScore = 0;
+    oScore = 0;
+    for (int i = 0; i < 9; i++) {
+      displayElement[i] = '';
+    }
+
+    filledBoxes = 0;
+  }
+
